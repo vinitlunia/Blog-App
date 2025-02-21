6e072143e17599ec0aa3ae9ae2b9945a7f644aea
vinitlunia <vinitlunia35@gmail.com>
2025-02-21 11:22:08 +0530

    updated 404

--- backend/index.js
+++ backend/index.js
@@ -14,13 +14,18 @@ const port = process.env.PORT
 const MONGO_URL = process.env.MONGO_URI
 // console.log(MONGO_URL);
 
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://blog-app-weld-eight-47.vercel.app"
+  ];
+
 
 //medelware..
 app.use(express.json())
 app.use(cookieParser());
 app.use(
     cors({ 
-    origin: process.env.FRONTEND_URL,
+    origin:  allowedOrigins,
     credentials: true,
     methods:["GET","POST","PUT","DELETE","PATCH"],
 
--- backend/jwt/AuthToken.js
+++ backend/jwt/AuthToken.js
@@ -4,12 +4,12 @@ import { User } from '../model/user.model.js'
 
 const createTokenAndSaveCookies = async (userId, res) => {
   const token = jwt.sign({ userId }, process.env.JWT_SECRET_KEY, {
-    expiresIn: "30d",
+    expiresIn: "60d",
   });
   res.cookie("jwt", token, {
-    httpOnly: true, // Temporarily set to false for testing
-    secure: false,
-    sameSite: "lax",
+    httpOnly: flase, // Temporarily set to false for testing
+    secure: true,
+    sameSite: "none",
     path: "/", // Ensure the cookie is available throughout the site
   });
   await User.findByIdAndUpdate(userId, { token });
